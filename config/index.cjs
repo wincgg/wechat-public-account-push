31cc932b0b9ee338b25e1767a480af695faa9f12
wincg <61812909+wincgg@users.noreply.github.com>
2023-12-31 14:41:59 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'okQn56A-s9gYyiVEDV1gEdTTRW4g',
+      id: 'okQn56LePlWmYbHq0I1gXtpSjxDg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'ntQFKxAnP596WOxKU7iARcg8viF6isxMO3Ce_uPuD90',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
